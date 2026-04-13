4416385c071549504304454622724b200333c71d
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2026-04-13 10:20:57 -0400

    chore: bump SW cache to v11 for fill-out tab

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yumyums-v10';
+const CACHE = 'yumyums-v11';
 const ASSETS = ['./', './index.html', './purchasing.html', './users.html', './login.html', './workflows.html', './ptr.js', './manifest.json'];
 
 self.addEventListener('install', e => {
