3dcf029c475ef7c1b98074963315209e536cb933
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2026-04-13 12:34:02 -0400

    chore(03-02): bump sw.js cache to v21 before human-verify checkpoint
    - Bumped yumyums-v20 to yumyums-v21 to invalidate cached pre-approval-flow version
    - INTG-01 verified: index.html Operations tile links to workflows.html (no change needed)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yumyums-v20';
+const CACHE = 'yumyums-v21';
 const ASSETS = ['./', './index.html', './purchasing.html', './users.html', './login.html', './workflows.html', './ptr.js', './manifest.json'];
 
 self.addEventListener('install', e => {
